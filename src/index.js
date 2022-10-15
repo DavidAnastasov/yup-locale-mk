24cf77837198ab94e8da6306681e3b73348e4daa
DavidAnastasov <davidanastasov@hotmail.com>
2022-10-15 21:50:43 +0200

    feat: Setup basic file structure

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import localeDefault from "./variants/localeDefault";
+import localeForm from "./variants/localeForm";
+import localeShort from "./variants/localeShort";
+
+export const mkLocale = localeDefault;
+export const mkForm = localeForm;
+export const mkShort = localeShort;
+
+export default {
+  mkLocale,
+  mkForm,
+  mkShort,
+};
