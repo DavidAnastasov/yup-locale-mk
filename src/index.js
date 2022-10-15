fb20485aedfd63d96ef9f11dd516fbe59d721912
DavidAnastasov <davidanastasov@hotmail.com>
2022-10-15 21:52:00 +0200

    refactor: Rename default variant

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import localeDefault from "./variants/localeDefault";
+import locale from "./variants/locale";
 import localeForm from "./variants/localeForm";
 import localeShort from "./variants/localeShort";
 
-export const mkLocale = localeDefault;
+export const mk = locale;
 export const mkForm = localeForm;
 export const mkShort = localeShort;
 
 export default {
-  mkLocale,
+  mk,
   mkForm,
   mkShort,
 };
